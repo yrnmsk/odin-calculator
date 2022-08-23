cb2ca437f5cb92f0e433a893a5ae095e7a3650a0
Youka <72034970+yrnmsk@users.noreply.github.com>
2022-08-23 22:29:16 +0800

    feat: add draft progress
    * feat: add html comments for temporary guide
    * feat: add debug css
    * feat: add and rename css/js id's and class names
    * feat: modify draft dynamic render a bit

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-const numbers = document.querySelector('#numbers');
+const buttons = document.querySelector('#buttons');
 
-// for (let i = 0; i < 10; ++i) {
-//   const number = document.createElement('div');
-//   number.classList.add('number');
-//   number.textContent = i;
-//   numbers.appendChild(number);
-// }
-
-let i, j, k = 0;
+const numbers = document.createElement('div');
+let i;
+let j;
+let k = 10;
+numbers.id = 'numbers';
 for (i = 1; i <= 4; ++i) {
   const row = document.createElement('div');
   row.classList.add('row');
   for (j = 1; j <= 3; ++j) {
-    const number = document.createElement('div');
-    number.classList.add('number');
-    number.textContent = ++k != 10 ? k : 0;
-    row.appendChild(number);
-    if (k == 10) break;
+    const button = document.createElement('div');
+    button.classList.add('button');
+    switch (--k) {
+      case -2:
+        button.textContent = 0;
+        break;
+      case -1:
+        button.textContent = '00';
+        break;
+      case 0:
+        button.textContent = '.';
+        break;
+      default:
+        button.textContent = k;
+        break;
+    }
+    row.appendChild(button);
   }
+  row.append(...Array.from(row.childNodes).reverse());
   numbers.appendChild(row);
 }
+buttons.appendChild(numbers);
